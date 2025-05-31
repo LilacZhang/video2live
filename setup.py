b04a1d0686ff224b6765bd447b9ce05f44794b99
SherlockOuO <wdf.coder@gmail.com>
2025-05-31 19:21:17 +0800

    feat: support convert video to live photo

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="video2live",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[
+        "imageio>=2.9.0",
+        "Pillow>=9.0.0",
+        "imageio-ffmpeg>=0.4.7",
+        "makelive>=0.6.2",  # 添加 makelive 依赖
+    ],
+    entry_points={
+        "console_scripts": [
+            "video2live = video2live.__main__:main",
+        ],
+    },
+    author="SherlockOuO",
+    author_email="wdf.coder@gmail.com",
+    description="Convert videos to iOS Live Photos",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/sherlockouo/video2live",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
