74f2781e3a53066a449a1569ab420a75fabcdbc4
SherlockOuO <wdf.coder@gmail.com>
2025-09-17 20:34:50 +0800

    Release version 0.0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="video2live",
-    version="0.0.4",
+    version="0.0.6",
     packages=find_packages(),
     install_requires=[
         "moviepy>=2.1.1",
